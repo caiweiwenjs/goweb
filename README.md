6536d71eb9f8953b8a09c1090aab64f715eb8516
weiwen.cww <weiwen.cww@alibaba-inc.com>
2019-02-23 20:22:30 +0800

    Simple web server

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # goweb
+
+## Prequisitis
+
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "log"
+)
+
+func main() {
+  http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Welcome to my website!")
+  })
+
+  fs := http.FileServer(http.Dir("static/"))
+  http.Handle("static/", http.StripPrefix("/static/", fs))
+
+  log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+
